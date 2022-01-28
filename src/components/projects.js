838bcf64743966e6675c81a3225fa2dedb34481b
KasperLunaIICS <70871444+KasperLunaIICS@users.noreply.github.com>
2022-01-28 22:40:19 +0800

    refactored projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,7 +16,7 @@ import { FaGithub } from "react-icons/fa";
 import Createev from "../logos/createev.js";
 import ThisSiteLogo from "../logos/thisSiteLogo.js";
 
-function PriceWrapper({ children }) {
+function PriceWrapper(props, { children }) {
   return (
     <Fade in={true} whileHover={{ scale: 1.05 }}>
       <Box
@@ -27,8 +27,41 @@ function PriceWrapper({ children }) {
         alignSelf={{ base: "center", lg: "flex-start" }}
         borderColor={useColorModeValue("gray.200", "gray.500")}
         borderRadius={"xl"}
+        textAlign={"center"}
       >
-        {children}
+        <Box py={4} px={7}>
+          <Text fontWeight="500" fontSize="2xl">
+            {props.title}
+          </Text>
+          <Text fontSize={"md"}>{props.Tag} </Text>
+          {props.Image}
+        </Box>
+        <VStack
+          bg={useColorModeValue("gray.50", "gray.700")}
+          py={4}
+          borderBottomRadius={"xl"}
+        >
+          <Box w={60}>
+            <Text color={"gray.500"} fontSize={"md"}>
+              {props.Description}
+            </Text>
+          </Box>
+          <Box pt={3}>
+            <Tooltip
+              openDelay={500}
+              label="Browse the Github Repo"
+              aria-label="A tooltip"
+            >
+              <IconButton
+                as={"a"}
+                colorScheme={"gray"}
+                href={props.GitLink}
+                target={"_blank"}
+                icon={<FaGithub size={25} />}
+              />
+            </Tooltip>
+          </Box>
+        </VStack>
       </Box>
     </Fade>
   );
@@ -46,125 +79,41 @@ export default function Projects(props) {
         </Text>
       </VStack>
       <Wrap w={"95%"} pt={9} spacing={8} justify="center">
-        <PriceWrapper>
-          <Box py={4} px={7}>
-            <Text fontWeight="500" fontSize="2xl">
-              Createev
-            </Text>
-            <Text fontSize={"md"}> e-Commerce Concept Site </Text>
-            <Createev colormode={props.colormode} />
-          </Box>
-          <VStack
-            bg={useColorModeValue("gray.50", "gray.700")}
-            py={4}
-            borderBottomRadius={"xl"}
-          >
-            <Box w={60}>
-              <Text color={"gray.500"} fontSize={"md"}>
-                Developed backend processes for the Createev e-commerce concept
-                site. Utilizing Node.js, Express.js and a MySQL database to
-                serve templated pages for e-commerce.
-              </Text>
+        <PriceWrapper
+          title={"Createev"}
+          Tag={"e-Commerce Concept Site "}
+          Image={<Createev colormode={props.colormode} />}
+          Description={`Developed backend processes for the Createev e-commerce concept
+          site. Utilizing Node.js, Express.js and a MySQL database to
+          serve templated pages for e-commerce.`}
+          GitLink={"https://github.com/KasperLuna/Createev-Concept"}
+        />
+        <PriceWrapper
+          title={"Dealcrafter"}
+          Tag={"Internal Inventory Management"}
+          Image={
+            <Box width="280px" height="280px">
+              <Image
+                alt={"Dealcrafter"}
+                src="/dealcrafter.png"
+                objectFit={"contain"}
+              />
             </Box>
-            <Box w="80%" pt={3}>
-              <Tooltip
-                openDelay={500}
-                label="Browse the Github Repo"
-                aria-label="A tooltip"
-              >
-                <IconButton
-                  as={"a"}
-                  colorScheme={"gray"}
-                  href={"https://github.com/KasperLuna/Createev-Concept"}
-                  target={"_blank"}
-                  icon={<FaGithub size={25} />}
-                />
-              </Tooltip>
-            </Box>
-          </VStack>
-        </PriceWrapper>
-        <PriceWrapper>
-          <Box position="relative">
-            <Box py={4} px={7}>
-              <Text fontWeight="500" fontSize="2xl">
-                Dealcrafter
-              </Text>
-              <Text fontSize={"md"}> Internal Inventory Management </Text>
-              <Box width="280px" height="280px">
-                <Image
-                  alt={"Dealcrafter"}
-                  src="/dealcrafter.png"
-                  objectFit={"contain"}
-                />
-              </Box>
-            </Box>
-            <VStack
-              bg={useColorModeValue("gray.50", "gray.700")}
-              py={4}
-              borderBottomRadius={"xl"}
-            >
-              <Box w={60}>
-                <Text color={"gray.500"} fontSize={"md"}>
-                  Developed backend processes for the internal Dealcrafter site,
-                  utilizing Node.js, Express.js, PostgreSQL, and Redis to serve
-                  templated pages used for inventory management.
-                </Text>
-              </Box>
-              <Box w="80%" pt={3}>
-                <Tooltip
-                  openDelay={500}
-                  label="Browse the Github Repo"
-                  aria-label="A tooltip"
-                >
-                  <IconButton
-                    as={"a"}
-                    colorScheme={"gray"}
-                    href={"https://github.com/KasperLuna/Dealcrafter"}
-                    target={"_blank"}
-                    icon={<FaGithub size={25} />}
-                  />
-                </Tooltip>
-              </Box>
-            </VStack>
-          </Box>
-        </PriceWrapper>
-        <PriceWrapper>
-          <Box py={4} px={7}>
-            <Text fontWeight="500" fontSize="2xl">
-              This Site
-            </Text>
-            <Text fontSize={"md"}> Portfolio Site </Text>
-            <ThisSiteLogo colormode={props.colormode} />
-          </Box>
-          <VStack
-            bg={useColorModeValue("gray.50", "gray.700")}
-            py={4}
-            borderBottomRadius={"xl"}
-          >
-            <Box w={60}>
-              <Text color={"gray.500"} fontSize={"md"}>
-                An ongoing project currently using Chakra UI and Next.js to
-                develop React.js skills and other skills related to Frontend
-                development and integrating such with Node.js-based API&apos;s.
-              </Text>
-            </Box>
-            <Box w="80%" pt={3}>
-              <Tooltip
-                openDelay={500}
-                label="Browse the Github Repo"
-                aria-label="A tooltip"
-              >
-                <IconButton
-                  as={"a"}
-                  colorScheme={"gray"}
-                  href={"https://github.com/KasperLuna/Next-Chakra-Portfolio  "}
-                  target={"_blank"}
-                  icon={<FaGithub size={25} />}
-                />
-              </Tooltip>
-            </Box>
-          </VStack>
-        </PriceWrapper>
+          }
+          Description={
+            "Developed backend processes for the internal Dealcrafter site, utilizing Node.js, Express.js, PostgreSQL, and Redis to serve templated pages used for inventory management."
+          }
+          GitLink={"https://github.com/KasperLuna/Dealcrafter"}
+        />
+        <PriceWrapper
+          title={"This Site"}
+          Tag={"Portfolio Site "}
+          Image={<ThisSiteLogo colormode={props.colormode} />}
+          Description={
+            "An ongoing project currently using Chakra UI and Next.js to develop React.js skills and other skills related to Frontend development and integrating such with Node.js-based API's."
+          }
+          GitLink={"https://github.com/KasperLuna/Next-Chakra-Portfolio"}
+        />
       </Wrap>
     </VStack>
   );
