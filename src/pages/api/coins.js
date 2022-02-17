aa50d398451a903cb4a41670f663f10b04064a3f
KasperLunaIICS <dev@kasperluna.com>
2022-02-17 15:21:13 +0800

    added crude coin search

--- src/pages/api/coins.js
+++ src/pages/api/coins.js
@@ -1,5 +1,9 @@
 export default async function handler(request, response) {
-    const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ccardano%2Cpolkadot%2Calgorand%2Cchainlink%2Ctezos%2Charmony&vs_currencies=usd');
-    const data = await res.json();
-    return response.status(200).json({ data });
-  }
+  const params = request["query"]["tickers"];
+  console.log(params);
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/simple/price?ids=${params}&vs_currencies=usd`
+  );
+  const data = await res.json();
+  return response.status(200).json(data);
+}
